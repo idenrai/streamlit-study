c21aa39dd6bed76e48b2a968bfc5e63c51dd6018
idenrai <geniuskwon966@gmail.com>
2023-10-25 04:08:59 +0900

    fix

--- streamlit/iris.py
+++ streamlit/iris.py
@@ -7,9 +7,7 @@ import streamlit as st
 iris_dataset = load_iris()
 
 df = pd.DataFrame(data=iris_dataset.data, columns=iris_dataset.feature_names)
-df.columns = [
-    col_name.split(" (cm)")[0] for col_name in df.columns
-]  # 컬럼명을 뒤에 cm 제거하였습니다
+df.columns = [col_name.split(" (cm)")[0] for col_name in df.columns]
 df["species"] = iris_dataset.target
 
 
