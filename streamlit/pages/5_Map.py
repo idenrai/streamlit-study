a6861208e8dc98f482d3391b1f81db9655d6405b
idenrai <geniuskwon966@gmail.com>
2023-10-26 09:05:38 +0900

    [update] session_state

--- streamlit/pages/5_Map.py
+++ streamlit/pages/5_Map.py
@@ -6,11 +6,26 @@ import pandas as pd
 # https://docs.streamlit.io/library/api-reference/charts/st.map
 # st.map(data=None, *, latitude=None, longitude=None, color=None, size=None, zoom=None, use_container_width=True)
 
-base_position = [35.542635, 139.445226]
+st.session_state.lat = st.sidebar.number_input(
+    label="lat",
+    step=0.000001,
+    value=35.542635,
+    format="%f",
+)
+st.session_state.lon = st.sidebar.number_input(
+    label="lon",
+    step=0.000001,
+    value=139.445226,
+    format="%f",
+)
+
+base_position = [st.session_state["lat"], st.session_state["lon"]]
 
 
 map_data = pd.DataFrame(
-    np.random.randn(5, 1) / [20, 20] + base_position, columns=["lat", "lon"]
+    np.random.randn(1, 1) / [20, 20] + base_position,
+    # base_position,
+    columns=["lat", "lon"],
 )
 
 print(map_data)
@@ -18,4 +33,5 @@ print(map_data)
 st.code("st.map(map_data)")
 
 st.subheader("Map")
+
 st.map(map_data)
